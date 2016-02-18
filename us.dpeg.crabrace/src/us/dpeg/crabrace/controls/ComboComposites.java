88ccf3331b65333ab0a6009688a00fc4e53156be
Donald Purnhagen <dpurnhagen@gmail.com>
2016-02-18 17:01:26 -0500

    Initial commit.

---
+++ us.dpeg.crabrace/src/us/dpeg/crabrace/controls/ComboComposites.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2016 Donald Purnhagen
+ *
+ * Description:
+ *     Set of compoaite panels controlled by a combo selection.
+ *
+ * Contributors:
+ *     Donald Purnhagen <dpurnhagen@gmail.com>
+ */
+package us.dpeg.crabrace.controls;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+/**
+ * Maintain a list of composites, which overlay the same space on the page.
+ * Enabling visibility by a key string in a combo box. Create the
+ * ComboComposite object, with or without a custom label for the combo box.
+ * Then use {@link #getComposite(String) getComposite(key)} to add or
+ * retrieve a composite. Use {@link #setSelection(String) setSelection(key)}
+ * to force the display of a particular composite.
+ *
+ * <h2>Example of usage:</h2>
+ * <pre>
+ * {@code
+ *	ComboComposites cc = new ComboComposites(parent);
+ *	cc.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+ *	Composite cAgility = cc.getComposite(AGILITY_KEY);
+ *	Composite cOwner = cc.getComposite(OWNER_KEY);
+ *	cAgility.setLayout(new GridLayout(1, false));
+ *	cOwner.setLayout(new GridLayout(2, false));
+ *	// Button for the Agility form
+ *	btnNimble = new Button(cAgility, SWT.CHECK);
+ *	btnNimble.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+ *	btnNimble.setText(AGILITY_TITLE);
+ *	// Label for the Owner form
+ *	Label lblOwner = new Label(cOwner, SWT.NONE);
+ *	lblOwner.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+ *	lblOwner.setText(OWNER_TITLE);
+ *	// Text field Label for the Owner form
+ *	txtOwner = new Text(cOwner, SWT.SINGLE | SWT.BORDER);
+ *	GridData gdTxtOwner = new GridData(SWT.FILL, SWT.CENTER, true, false);
+ *	txtOwner.setLayoutData(gdTxtOwner);
+ *	// Show the Agility form
+ *	cc.setSelection(AGILITY_KEY);
+ * }
+ * </pre>
+ *
+ * @author Don Purnhagen
+ */
+public class ComboComposites extends Composite {
+	private Map<String, Composite> map;
+	private String label;
+	private Combo comboCombo;
+	private String selection;
+
+	/**
+	 * @param parent the parent container
+	 *
+	 * @wbp.parser.constructor
+	 */
+	public ComboComposites(Composite parent) {
+		this(parent, "Pick One:");
+	}
+
+	/**
+	 * @param parent the parent container
+	 * @param label the text label for the combo box
+	 */
+	public ComboComposites(Composite parent, String label) {
+		super(parent, SWT.NONE);
+		map = new HashMap<String, Composite>();
+		this.label = label;
+		this.selection = ""; //$NON-NLS-1$
+
+		setLayout(new GridLayout(2, false));
+
+		Label lblLabel = new Label(this, SWT.NONE);
+		lblLabel.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false, 1, 1));
+		lblLabel.setText(label);
+
+		comboCombo = new Combo(this, SWT.READ_ONLY);
+		comboCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		comboCombo.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				setSelection(((Combo) e.widget).getText());
+			}
+		});
+
+	}
+
+	protected void hideComposite(Composite composite, boolean hide) {
+		GridData gridData = (GridData) composite.getLayoutData();
+		gridData.exclude = hide;
+		composite.setVisible(!hide);
+	}
+
+	/**
+	 * @param key the unique string identifier and text for the combo box entry
+	 * @return the composite associated with the key
+	 */
+	public Composite getComposite(String key) {
+		if (map.containsKey(key)) {
+			return map.get(key);
+		}
+		final Composite composite = new Composite(this, SWT.None);
+		final GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1);
+		composite.setLayoutData(gd);
+		gd.exclude = true;
+		map.put(key, composite);
+		comboCombo.add(key);
+		return composite;
+	}
+
+	/**
+	 * @return the label
+	 */
+	public String getLabel() {
+		return label;
+	}
+
+	/**
+	 * @return the map
+	 */
+	protected Map<String, Composite> getMap() {
+		return map;
+	}
+
+	/**
+	 * @return the selection
+	 */
+	public String getSelection() {
+		return selection;
+	}
+
+	/**
+	 * @param label the label to set
+	 */
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	/**
+	 * @param map the map to set
+	 */
+	protected void setMap(Map<String, Composite> map) {
+		this.map = map;
+	}
+
+	/**
+	 * The composite associated with the key will be displayed. All other
+	 * composites will be hidden.
+	 *
+	 * @param selection the key to select in the combo box
+	 */
+	public void setSelection(String key) {
+		if (!key.equals(selection)) {
+			// hide currently selected composite if it exists
+			if (map.containsKey(selection)) {
+				Composite c = map.get(selection);
+				if (c != null) {
+					hideComposite(c, true);
+				}
+			}
+			// show newly selected composite if it exists
+			if (map.containsKey(key)) {
+				Composite c = map.get(key);
+				if (c != null) {
+					hideComposite(c, false);
+				}
+			}
+			this.layout();
+			// save the newly selected key
+			this.selection = key;
+		}
+		comboCombo.setText(selection);
+	}
+}
---
+++ us.dpeg.crabrace/src/us/dpeg/crabrace/properties/CrabraceProperties.java
@@ -0,0 +1,188 @@
+package us.dpeg.crabrace.properties;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.QualifiedName;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Scale;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.PropertyPage;
+
+import us.dpeg.crabrace.controls.ComboComposites;
+
+public class CrabraceProperties extends PropertyPage {
+
+	private static final String LOOKS_DEFAULT = "5";
+	private static final String AGILITY_DEFAULT = "false";
+	private static final String PATH_TITLE = "Path:";
+	private static final String AGILITY_TITLE = "&Extra nimble";
+	private static final String AGILITY_PROPERTY = "AGILITY";
+	private static final String AGILITY_KEY = "Agility";
+	private static final String LOOKS_TITLE = "&Looks Factor:";
+	private static final String LOOKS_PROPERTY = "LOOKS";
+	private static final String LOOKS_KEY = "Looks";
+	private static final String OWNER_TITLE = "&Owner:";
+	private static final String OWNER_PROPERTY = "OWNER";
+	private static final String OWNER_KEY = "Owner";
+	private static final String OWNER_DEFAULT = "John Doe";
+
+	private Text txtOwner;
+	private Button btnNimble;
+	private Scale scaleLooks;
+
+	/**
+	 * Constructor for SamplePropertyPage.
+	 */
+	public CrabraceProperties() {
+		super();
+	}
+
+	private void addFirstSection(Composite parent) {
+		Composite composite = createDefaultComposite(parent);
+
+		//Label for path field
+		Label pathLabel = new Label(composite, SWT.NONE);
+		pathLabel.setText(PATH_TITLE);
+
+		// Path text field
+		Text pathValueText = new Text(composite, SWT.WRAP | SWT.READ_ONLY);
+		pathValueText.setText(((IResource) getElement()).getFullPath().toString());
+	}
+
+	private void addSeparator(Composite parent) {
+		Label separator = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
+		GridData gridData = new GridData();
+		gridData.horizontalAlignment = GridData.FILL;
+		gridData.grabExcessHorizontalSpace = true;
+		separator.setLayoutData(gridData);
+	}
+
+	private void addSecondSection(Composite parent) {
+		ComboComposites cc = new ComboComposites(parent);
+		cc.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+		Composite cAgility = cc.getComposite(AGILITY_KEY);
+		Composite cLooks = cc.getComposite(LOOKS_KEY);
+		Composite cOwner = cc.getComposite(OWNER_KEY);
+
+		cAgility.setLayout(new GridLayout(1, false));
+		cLooks.setLayout(new GridLayout(3, false));
+		cOwner.setLayout(new GridLayout(2, false));
+
+		// Button for agility
+		btnNimble = new Button(cAgility, SWT.CHECK);
+		btnNimble.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+		btnNimble.setText(AGILITY_TITLE);
+
+		// Label for looks field
+		Label lblLooks = new Label(cLooks, SWT.NONE);
+		lblLooks.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false, 3, 1));
+		lblLooks.setText(LOOKS_TITLE);
+
+		// Slider for looks field
+		Label lblUgly = new Label(cLooks, SWT.None);
+		lblUgly.setText("Ugly");
+		scaleLooks = new Scale(cLooks, SWT.HORIZONTAL);
+		scaleLooks.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		scaleLooks.setMinimum(1);
+		scaleLooks.setMaximum(10);
+		scaleLooks.setIncrement(1);
+		scaleLooks.setSelection(5);
+		Label lblSexy = new Label(cLooks, SWT.None);
+		lblSexy.setText("Sexy");
+
+		// Label for owner field
+		Label lblOwner = new Label(cOwner, SWT.NONE);
+		lblOwner.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+		lblOwner.setText(OWNER_TITLE);
+
+		// Owner text field
+		txtOwner = new Text(cOwner, SWT.SINGLE | SWT.BORDER);
+		GridData gdTxtOwner = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		//gdTxtOwner.widthHint = convertWidthInCharsToPixels(50);
+		txtOwner.setLayoutData(gdTxtOwner);
+
+		// Populate fields
+		IResource element = (IResource) getElement();
+		try {
+			String agility = ((IResource) getElement()).getPersistentProperty(new QualifiedName("", AGILITY_PROPERTY));
+			btnNimble.setSelection(Boolean.parseBoolean((agility != null) ? agility : AGILITY_DEFAULT));
+		} catch (CoreException e) {
+			btnNimble.setSelection(Boolean.parseBoolean(AGILITY_DEFAULT));
+		}
+		try {
+			String looks = ((IResource) getElement()).getPersistentProperty(new QualifiedName("", LOOKS_PROPERTY));
+			scaleLooks.setSelection(Integer.parseInt((looks != null) ? looks : LOOKS_DEFAULT));
+		} catch (CoreException e) {
+			scaleLooks.setSelection(Integer.parseInt(LOOKS_DEFAULT));
+		}
+		try {
+			String owner = element.getPersistentProperty(new QualifiedName("", OWNER_PROPERTY));
+			txtOwner.setText((owner != null) ? owner : OWNER_DEFAULT);
+		} catch (CoreException e) {
+			txtOwner.setText(OWNER_DEFAULT);
+		}
+
+		cc.setSelection(AGILITY_KEY);
+	}
+
+	/**
+	 * @see PreferencePage#createContents(Composite)
+	 */
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridLayout layout = new GridLayout();
+		composite.setLayout(layout);
+		GridData data = new GridData(GridData.FILL);
+		data.grabExcessHorizontalSpace = true;
+		composite.setLayoutData(data);
+
+		addFirstSection(composite);
+		addSeparator(composite);
+		addSecondSection(composite);
+		return composite;
+	}
+
+	private Composite createDefaultComposite(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		composite.setLayout(layout);
+
+		GridData data = new GridData();
+		data.verticalAlignment = GridData.FILL;
+		data.horizontalAlignment = GridData.FILL;
+		composite.setLayoutData(data);
+
+		return composite;
+	}
+
+	protected void performDefaults() {
+		super.performDefaults();
+		// Populate the controls with the default values
+		btnNimble.setSelection(Boolean.parseBoolean(AGILITY_DEFAULT));
+		scaleLooks.setSelection(Integer.parseInt(LOOKS_DEFAULT));
+		txtOwner.setText(OWNER_DEFAULT);
+	}
+
+	public boolean performOk() {
+		// store the value in the owner text field
+		IResource element = (IResource) getElement();
+		try {
+			element.setPersistentProperty(new QualifiedName("", AGILITY_PROPERTY), String.valueOf(btnNimble.getSelection()));
+			element.setPersistentProperty(new QualifiedName("", LOOKS_PROPERTY), String.valueOf(scaleLooks.getSelection()));
+			element.setPersistentProperty(new QualifiedName("", OWNER_PROPERTY), txtOwner.getText());
+		} catch (CoreException e) {
+			return false;
+		}
+		return true;
+	}
+
+}
